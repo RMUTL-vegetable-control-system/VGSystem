9c92be264ef7e850f83fd155a5b42535be93021b
mrnonny416 <mrnonny416@gmail.com>
2022-06-13 14:06:36 +0700

    Ex Navigation on App.js

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
+// import { StatusBar } from 'expo-status-bar';
+// import { StyleSheet, Text, View } from 'react-native';
+// import { NavigationContainer } from '@react-navigation/native';
+// import { createNativeStackNavigator } from '@react-navigation/native-stack';
+// import MAIN from './screens/Main/Main';
+
+// const Stack = createNativeStackNavigator();
+
+// export default function App() {
+//   return (
+//     <>
+//       <NavigationContainer>
+//         <Stack.Navigator>
+
+//         </Stack.Navigator>
+//       </NavigationContainer>
+//     </>
+
+//   );
+// }
+
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
+const Stack = createStackNavigator();
+
+function HomeScreen() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen</Text>
     </View>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+function App() {
+  const ref = React.useRef(null);
+
+  return (
+    <View style={{ flex: 1 }}>
+      <NavigationContainer ref={ref}>
+        <Stack.Navigator initialRouteName="Empty">
+          <Stack.Screen name="Empty" component={() => <View></View>} />
+          <Stack.Screen name="Home" component={HomeScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+      <Button
+        onPress={() => ref.current && ref.current.navigate('Home')}
+        title="Go home"
+      />
+    </View>
+  );
+}
+
+export default App;
