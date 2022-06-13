da7bc8908afe169815436412cdf08edba424bfac
icerittikhai35 <ritthikai.khan@gmai.com>
2022-06-13 14:26:08 +0700

    Ex Import Stack navigation and import useRef

--- App.js
+++ App.js
@@ -18,9 +18,9 @@
 
 //   );
 // }
-
+import { useRef } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,Button } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 
@@ -35,7 +35,7 @@ function HomeScreen() {
 }
 
 function App() {
-  const ref = React.useRef(null);
+  const ref = useRef(null);
 
   return (
     <View style={{ flex: 1 }}>
